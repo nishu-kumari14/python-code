d174656716e186eda5f05c48c717ae9cc49732b7
nishu-kumari14 <118430564+nishu-kumari14@users.noreply.github.com>
2022-12-04 16:22:04 +0530

    Add files via upload

---
+++ python class.py
@@ -0,0 +1,59 @@
+s="python programming"
+# p=s.isalpha()
+# print(p)
+# p=s.isdigit()
+# print(p)
+# p=s.isalnum
+# print(p)
+# p=s.isalpha()
+# print(p)
+# p=c.endswith("p")
+# print(p)
+# p=c.startswith("python")
+# print(p)
+# p=c.rfind("p")
+# print(p)
+# p=c.count("p")
+#print(p)
+# p=c.capitalize()
+# print(p)
+# p=c.lower()
+# print(p)
+# p=c.upper()
+# print(p)
+# p=c.title()
+# print(p)
+#p=c.replace()
+#print(p)
+# p=s.lstrip("p")
+# print(p)
+# p=s.rstrip("ing")
+#print(p)
+# s="hello world"
+# p=s.center(50)
+# print(p)
+# p=s.rjust(40)
+# print(p)
+#p=s.ljust(80)
+#print(p)
+# def count(b,c):
+#     a=0
+#     for i in b:
+#         if i==c:
+#             a+=1
+#         else:
+#             continue
+#     return a
+# b=input()
+#c=input()
+#print(count(b,c))
+s="python progamming"
+p=s.upper()
+print(p)
+for a in ['a','e','i','o','u','A','E','I','O','U']:
+    print(a,end=' ')
+
+i=input("enter to remove")
+p=s.replace(i,'')
+print(p)
+
